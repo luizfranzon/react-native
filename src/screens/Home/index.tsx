f07dbe88714efe9a8832b8d9012016c85b678497
Luiz Franzon <luizfranzon05@hotmail.com>
2024-01-21 14:52:30 -0300

    `add:` participant add form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,31 @@
-import { Text, View } from "react-native"
+import { Text, TextInput, View, TouchableOpacity } from "react-native"
 
 import { styles } from "./styles"
 
-export default function Home() {
+export function Home() {
+
+  function handleParticipantAdd() {
+    console.log("participante adicionado")
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <View>
-          <Text style={styles.text}>
-            Bem-vindo!
-          </Text>
-          <Text style={styles.subText}>20 de Janeiro de 2024</Text>
-        </View>
-        <Text style={styles.text}>Luiz F.</Text>
+      <Text style={styles.text}>
+        Bem-vindo!
+      </Text>
+
+      <Text style={styles.subText}>20 de Janeiro de 2024</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor={"#6B6B6B"}
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -15,8 +15,33 @@ export const styles = StyleSheet.create({
   subText: {
     color: 'white'
   },
-  header: {
+  input: {
+    backgroundColor: '#1F1E25',
+    height: 56,
+    borderRadius: 4,
+    color: '#FFF',
+    padding: 16,
+    fontSize: 16,
+    flex: 1,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  button: {
+    width: 54,
+    height: 54,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  form: {
+    width: '100%',
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    alignItems: 'center',
+    marginTop: 16,
+    gap: 10
   }
 })
